fa68b77478e3121ff8964555210ca3e32f915c6c
Fanzil <fan.hakimov2010@yandex.ru>
2020-03-04 17:37:06 +0500

    Remove class, split test_solve method

--- cramers_rule.py
+++ cramers_rule.py
@@ -1,106 +1,104 @@
+"""Solve SLAE using Cramer's rule."""
+
 from copy import deepcopy
 
 
-class Cramer:
-    """Solve SLAE using Cramer's rule."""
+def solve(arr_a, arr_b):
+    """Find the solution of the SLAE.
 
-    @staticmethod
-    def solve(arr_a, arr_b):
-        """Find the solution of the SLAE.
+    :param arr_a: The coefficient matrix of equations
+    :param arr_b: The column vector of right-hand-sides of equations
+    :return: X, the solution vector
+    """
+    det = get_det_if_valid(arr_a, arr_b)
 
-        :param arr_a: The coefficient matrix of equations
-        :param arr_b: The column vector of right-hand-sides of equations
-        :return: X, the solution vector
-        """
-        det = Cramer.get_det_if_valid(arr_a, arr_b)
+    arr_x = [
+        get_det(arr_replace(arr_a, arr_b, i)) / det
+        for i in range(len(arr_a))
+    ]
 
-        arr_x = [
-            Cramer.get_det(Cramer.arr_replace(arr_a, arr_b, i)) / det
-            for i in range(len(arr_a))
-        ]
+    return arr_x
 
-        return arr_x
 
-    @staticmethod
-    def get_det_if_valid(arr_a, arr_b):
-        """Get the determinant of an array A, if arrays are correct.
+def get_det_if_valid(arr_a, arr_b):
+    """Get the determinant of an array A, if arrays are correct.
 
-        :param arr_a: The coefficient matrix of equations
-        :param arr_b: The column vector of right-hand-sides of equations
-        :return: The determinant of an array A
-        """
-        if not (arr_a and arr_b):
-            raise ValueError(
-                "The matrix or the column vector of right-hand-sides of the "
-                "equations is empty!"
-            )
+    :param arr_a: The coefficient matrix of equations
+    :param arr_b: The column vector of right-hand-sides of equations
+    :return: The determinant of an array A
+    """
+    if not (arr_a and arr_b):
+        raise ValueError(
+            "The matrix or the column vector of right-hand-sides of the "
+            "equations is empty!"
+        )
 
-        size = len(arr_a)
-        if len(arr_b) != size:
-            raise ValueError(
-                "The size of the column vector of right-hand-sides of the "
-                "equations must be equal to the size of the matrix!"
-            )
+    size = len(arr_a)
+    if len(arr_b) != size:
+        raise ValueError(
+            "The size of the column vector of right-hand-sides of the "
+            "equations must be equal to the size of the matrix!"
+        )
 
-        if size != 1:
-            for arr in arr_a:
-                if len(arr) != size:
-                    raise ValueError("The matrix must be quadratic!")
+    if size != 1:
+        for arr in arr_a:
+            if len(arr) != size:
+                raise ValueError("The matrix must be quadratic!")
 
-        det = Cramer.get_det(arr_a)
-        if not det:
-            raise ValueError("The matrix determinant must not be 0!")
+    det = get_det(arr_a)
+    if not det:
+        raise ValueError("The matrix determinant must not be 0!")
 
-        return det
+    return det
 
-    @staticmethod
-    def get_det(arr):
-        """Get the determinant of an array.
 
-        :param arr: An array
-        :return: The array determinant
-        """
-        if len(arr) == 1:
-            return arr[0]
+def get_det(arr):
+    """Get the determinant of an array.
 
-        det = 0
-        for i, j in enumerate(arr[0]):
-            det += j * ((-1) ** i) * Cramer.get_det(Cramer.arr_remove(arr, i))
+    :param arr: An array
+    :return: The array determinant
+    """
+    if len(arr) == 1:
+        return arr[0]
 
-        return det
+    det = 0
+    for i, j in enumerate(arr[0]):
+        det += j * ((-1) ** i) * get_det(arr_remove(arr, i))
 
-    @staticmethod
-    def arr_remove(arr, index):
-        """Remove the 1st row and the index-th column from an array.
+    return det
 
-        :param arr: An array
-        :param index: An index
-        :return: A changed copy of the array
-        """
-        res = deepcopy(arr[1:])
 
-        for i in res:
-            i.pop(index)
+def arr_remove(arr, index):
+    """Remove the 1st row and the index-th column from an array.
 
-        if len(res) == 1:
-            return res[0]
-        return res
+    :param arr: An array
+    :param index: An index
+    :return: A changed copy of the array
+    """
+    res = deepcopy(arr[1:])
 
-    @staticmethod
-    def arr_replace(arr, col, index):
-        """Replace the index-th column from an array by a column vector.
+    for i in res:
+        i.pop(index)
 
-        :param arr: An array
-        :param col: A column vector
-        :param index: An index
-        :return: A changed copy of the array
-        """
-        if len(arr) == 1:
-            return col
+    if len(res) == 1:
+        return res[0]
+    return res
 
-        res = deepcopy(arr)
 
-        for i, j in enumerate(col):
-            res[i][index] = j
+def arr_replace(arr, col, index):
+    """Replace the index-th column from an array by a column vector.
 
-        return res
+    :param arr: An array
+    :param col: A column vector
+    :param index: An index
+    :return: A changed copy of the array
+    """
+    if len(arr) == 1:
+        return col
+
+    res = deepcopy(arr)
+
+    for i, j in enumerate(col):
+        res[i][index] = j
+
+    return res
--- test_cramers_rule.py
+++ test_cramers_rule.py
@@ -1,24 +1,24 @@
 from unittest import main, TestCase
 
-from cramers_rule import Cramer
+from cramers_rule import solve
 
 
-class TestCramer(TestCase):
-    def test_solve(self):
+class TestSolve(TestCase):
+    def test_correct_array(self):
         arr_a = [
             [2, 5, 4],
             [1, 3, 2],
             [2, 10, 9],
         ]
         arr_b = [30, 150, 110]
-        self.assertListEqual(Cramer.solve(arr_a, arr_b), [-152, 270, -254])
+        self.assertListEqual(solve(arr_a, arr_b), [-152, 270, -254])
 
         arr_a = [
             [1, -1],
             [3, 2],
         ]
         arr_b = [7, 16]
-        self.assertListEqual(Cramer.solve(arr_a, arr_b), [6, -1])
+        self.assertListEqual(solve(arr_a, arr_b), [6, -1])
 
         arr_a = [
             [2, -4, 1, -5],
@@ -27,7 +27,7 @@ class TestCramer(TestCase):
             [2, -3, 1, -1],
         ]
         arr_b = [2, -5, 3, 0]
-        self.assertListEqual(Cramer.solve(arr_a, arr_b), [1, 2, 3, -1])
+        self.assertListEqual(solve(arr_a, arr_b), [1, 2, 3, -1])
 
         arr_a = [
             [2, 5, 4],
@@ -35,19 +35,21 @@ class TestCramer(TestCase):
             [2, 10, 9],
         ]
         arr_b = [30, 150, 110]
-        self.assertListEqual(Cramer.solve(arr_a, arr_b), [-152, 270, -254])
+        self.assertListEqual(solve(arr_a, arr_b), [-152, 270, -254])
 
         arr_a = [2]
         arr_b = [14]
-        self.assertListEqual(Cramer.solve(arr_a, arr_b), [7])
+        self.assertListEqual(solve(arr_a, arr_b), [7])
 
+    def test_empty_array(self):
         with self.assertRaises(ValueError):
-            Cramer.solve([], [])
+            solve([], [])
 
+    def test_wrong_size_array(self):
         arr_a = [2]
         arr_b = [14, 4]
         with self.assertRaises(ValueError):
-            Cramer.solve(arr_a, arr_b)
+            solve(arr_a, arr_b)
 
         arr_a = [
             [1, 4],
@@ -55,7 +57,7 @@ class TestCramer(TestCase):
         ]
         arr_b = [6, 5, 0]
         with self.assertRaises(ValueError):
-            Cramer.solve(arr_a, arr_b)
+            solve(arr_a, arr_b)
 
         arr_a = [
             [1, 4],
@@ -64,15 +66,26 @@ class TestCramer(TestCase):
         ]
         arr_b = [6, 5, 0]
         with self.assertRaises(ValueError):
-            Cramer.solve(arr_a, arr_b)
+            solve(arr_a, arr_b)
 
+    def test_non_quadratic_array(self):
+        arr_a = [
+            [1, 5],
+            [13, 60],
+            [14, 0],
+        ]
+        arr_b = [15, 17]
+        with self.assertRaises(ValueError):
+            solve(arr_a, arr_b)
+
+    def test_array_with_zero_determinant(self):
         arr_a = [
             [1, -2],
             [-2, 4],
         ]
         arr_b = [6, 0]
         with self.assertRaises(ValueError):
-            Cramer.solve(arr_a, arr_b)
+            solve(arr_a, arr_b)
 
 
 if __name__ == "__main__":
